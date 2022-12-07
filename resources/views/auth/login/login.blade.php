c92c89d9143e651da6062d1894f07485e998890f
大沢亮輔 <>
2022-12-07 18:35:34 +0900

    “ログインタイムアウトのページ遷移”

--- resources/views/auth/login/login.blade.php
+++ resources/views/auth/login/login.blade.php
@@ -14,6 +14,7 @@
 </head>
 <body>
   <form action="{{ route('loginPost') }}" method="POST">
+    @csrf
     <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
       <div class="border vh-50 w-25">
         <div class="w-75 m-auto pt-5">
