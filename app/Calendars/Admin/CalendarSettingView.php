4821f9a378cad2041a62b77b50647829979eb566
大沢亮輔 <>
2022-12-20 10:50:45 +0900

    “予約画面の表示形式を変更”

--- app/Calendars/Admin/CalendarSettingView.php
+++ app/Calendars/Admin/CalendarSettingView.php
@@ -39,7 +39,7 @@ class CalendarSettingView{
         $startDay = $this->carbon->format("Y-m-01");
         $toDay = $this->carbon->format("Y-m-d");
 
-       if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
+      if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
           $html[] = '<td class="past-day border">';
         }else{
           $html[] = '<td class="border '.$day->getClassName().'">';
